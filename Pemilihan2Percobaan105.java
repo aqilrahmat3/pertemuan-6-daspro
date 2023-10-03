402d21ab0232f7f70ad9143b7b07e74bdbc3254c
aqilrahmat3 <144086802+aqilrahmat3@users.noreply.github.com>
2023-10-03 11:27:56 +0700

    first commit

---
+++ Pemilihan2Percobaan105.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Pemilihan2Percobaan105 {
+    public static void main(String[] args) {
+        Scanner input05 = new Scanner(System.in);
+        System.out.println("Masukkan tahun");
+        int tahun = input05.nextInt();
+
+        if ((tahun % 4) == 0) {
+            if ((tahun % 100) != 0);
+               System.out.println("Tahun kabisat");
+        } else
+            System.out.println("Bukan tahun kabisat");
+    }
+}
